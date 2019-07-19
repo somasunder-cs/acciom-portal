48ead59fc9246591dd4e9baea1dcead2f79b9604
Nandan S <AL1453@accionlabs.in>
2019-07-19 17:38:24 +0530

    Dashboard Changes

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CardContainer from './CardContainers';
+import TimeLineComponent from './Timeline'
 
 class Dashboard extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Dashboard page ..</h1>
+				<CardContainer />
+				<TimeLineComponent />
 			</div>
 		)
 	}
---
+++ src/containers/card.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../css/card.css';
+
+
+function Cards () {
+    return (
+        <div>
+            <div className="row tile_count">
+				<div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+				<span className="count_top"><i className="fa fa-user"></i> Total Users</span>
+				<div className="count">2500</div>
+				<span className="count_bottom"><i className="green">4% </i> From last Week</span>
+				</div>
+				<div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+				<span className="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
+				<div className="count">123.50</div>
+				<span class="count_bottom"><i className="green"><i className="fa fa-sort-asc"></i>3% </i> From last Week</span>
+				</div>
+				<div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+				<span className="count_top"><i className="fa fa-user"></i> Total Males</span>
+				<div className="count green">2,500</div>
+				<span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"></i>34% </i> From last Week</span>
+				</div>
+				<div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+				<span className="count_top"><i className="fa fa-user"></i> Total Females</span>
+				<div className="count">4,567</div>
+				<span className="count_bottom"><i className="red"><i className="fa fa-sort-desc"></i>12% </i> From last Week</span>
+				</div>
+				<div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+				<span className="count_top"><i className="fa fa-user"></i> Total Collections</span>
+				<div className="count">2,315</div>
+				<span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"></i>34% </i> From last Week</span>
+				</div>
+				<div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+				<span className="count_top"><i className="fa fa-user"></i> Total Connections</span>
+				<div className="count">7,325</div>
+				<span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"></i>34% </i> From last Week</span>
+				</div>
+			</div>
+        </div>
+    );
+}   
+
+export default Cards;
