b912c7941ae04c865ccc955524300574b636b921
Nandan S <AL1453@accionlabs.in>
2019-08-22 19:21:29 +0530

    dashboard-changes

--- src/components/DonutChart.js
+++ src/components/DonutChart.js
@@ -11,77 +11,37 @@ class DonutChart extends Component {
 
 	render() {
 		this.options = {
-			optionsRadial: {
-				fill: {
-					type: 'solid',
-					colors: this.props.colors
-				},
-				chart : {
-					events: {
-						click : (event, chartContext, config) => {
-							this.props.getDQIprojectDetails(this.props.chartData.project_id);
-						}
-					}
-				},
-				plotOptions: {
-					radialBar: {
-						hollow: {
-							margin: 0,
-							size: "70%",
-							background: "#fff",
-							image: undefined,
-							imageOffsetX: 0,
-							imageOffsetY: 0,
-							position: "front",
-							dropShadow: {
-								enabled: true,
-								top: 3,
-								left: 0,
-								blur: 4,
-								opacity: 0.24
-							}
-						},
-						track: {
-							background: "#fff",
-							strokeWidth: "70%",
-							margin: -7, // margin is in pixels
-							dropShadow: {
-								enabled: true,
-								top: -3,
-								left: 0,
-								blur: 4,
-								opacity: 0.35
-							}
-						},
-
-						dataLabels: {
-							showOn: "always",
-							name: {
-								offsetY: -40,
-								show: true,
-								color: "#888",
-								fontSize: "13px"
-							},
-							value: {
-								formatter: (val) => {
-									return val + '%';
-								},
-								color: "#111",
-								fontSize: "18px",
-								show: true
-							}
-						}
-					}
-				},
-				labels: ['']
+			chart: {
+				height: 300,
+				type: 'radialBar',
 			},
-			seriesRadial: [this.props.chartData.project_dqi_percentage.toFixed(2)],
+			series: [this.props.chartData.project_dqi_percentage.toFixed(2)],
+			labels: ['DQI'],
+			colors: ['#E74B56'],
+			plotOptions: {
+				radialBar: {
+				  dataLabels: {
+					showOn: "always",
+					name: {
+					  offsetY: -5,
+					  show: true,
+					  color: "#888",
+					  fontSize: "20px",
+					},
+					value: {
+					  color: "#111",
+					  fontSize: "20px",
+					  show: true,
+					  offsetY: 5
+					}
+				  }
+				}
+			}
 		}
 		return (
 			<div className="donut">
-				<Chart options={this.options.optionsRadial} series={this.options.seriesRadial} colors={this.options.optionsRadial.colors} type="radialBar" width="250" />
-				<div className="chartLabel">DQI</div>
-				<span className="chartProjectName">{this.props.chartData.project_name}</span>
+				<Chart options={this.options} series={this.options.series} colors={this.options.colors} type="radialBar" width="300" />
+				<span className="chartProjectName">{this.props.chartData.project_name.split(" ")[0]}</span>
 			</div>
 		);
 	}
